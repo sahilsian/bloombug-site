7ab1882550d7bd47439da81bd1900e425f9284a0
sahilsian <sahilsian101@gmail.com>
2023-10-08 01:30:38 -0300

    .

--- components/ButtonLink/ButtonLink.js
+++ components/ButtonLink/ButtonLink.js
@@ -5,6 +5,6 @@ import Link from "next/link"
 export const ButtonLink = ({destination, label, target = "_self", color = "#FFFFFF", fontColor = "#C64B9B"}) => {
     return <Link target={target} href={destination} style={{backgroundColor: color, color: fontColor}} className="btn w-full">
     {label}
-    <FontAwesomeIcon style={{paddingLeft: "20px"}} icon={faArrowRight}></FontAwesomeIcon>
+    <FontAwesomeIcon style={{paddingLeft: "20px", fontSize: "16px"}} size="16px" icon={faArrowRight}></FontAwesomeIcon>
     </Link>
 }
