355d100384ccce55821544468c6d58d7539c776f
sahilsian <sahilsian101@gmail.com>
2024-03-19 14:03:40 -0300

    gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import StyledComponentsRegistry from '../lib/registry'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function Document({ children }) {
   return (
@@ -8,6 +9,7 @@ export default function Document({ children }) {
       <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
       <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
       </Head>
+      <GoogleAnalytics gaId="G-CHR0TKY6LB" />
       <body id={'root'}>
         <Main />
         <NextScript />
